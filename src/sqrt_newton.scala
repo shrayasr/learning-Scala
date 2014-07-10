1011f7d12f3c5878ef4a0e9eb694b0a61df18e31
Shrayas <shrayasr@gmail.com>
2014-07-11 00:27:55 +0530

    Improving sqrt function
    Learnings:
    - Lexical scope
    - Blocks

--- src/sqrt_newton.scala
+++ src/sqrt_newton.scala
@@ -2,21 +2,24 @@ object sqrt_newton {
 
   def abs(x:Double) = if (x<0) -x else x
 
-  def sqrtIter(guess: Double, x: Double) : Double = {
+  def sqrt(x:Double) = {
 
-    if (isGoodEnough(guess, x)) guess 
-    else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double) : Double = {
+
+      if (isGoodEnough(guess)) guess 
+      else sqrtIter(improve(guess))
+    }
+
+    def isGoodEnough(guess: Double) : Boolean = {
+      abs(guess*guess - x) / x < 0.001
+    }
+
+    def improve(guess: Double) : Double = {
+      (guess + x/guess) / 2
+    }
+
+    sqrtIter(1.0)
   }
-
-  def isGoodEnough(guess: Double, x: Double) : Boolean = {
-    abs(guess*guess - x) / x < 0.001
-  }
-
-  def improve(guess: Double, x: Double) : Double = {
-    (guess + x/guess) / 2
-  }
-
-  def sqrt(x: Double) = sqrtIter(1.0, x)
 
   def main(args: Array[String]) {
     println(sqrt(2))

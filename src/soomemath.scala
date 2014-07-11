0a513856e53912b6dd4957abe66141c289f13814
Shrayas <shrayasr@gmail.com>
2014-07-11 21:04:40 +0530

    Implementing a better factorial
    This factorial problem supports tail recursion, which is awesome because
    it uses the same stack frame.

--- src/soomemath.scala
+++ src/soomemath.scala
@@ -8,9 +8,18 @@ object soomemath {
     if (x==0) 1 else (x*factorial(x-1))
   }
 
+  def better_factorial(x: Integer): Integer = {
+    def loop(acc: Integer, n: Integer): Integer = {
+      if (n==0) acc
+      else loop(acc*n, n-1)
+    }
+    loop(1, x)
+  }
+
   def main(args: Array[String]) {
     println(gcd(48,18))
     println(factorial(5))
+    println(better_factorial(5))
   }
 
 }

3f44c9e240d5793a39f2ba18e50aa84fc2869d84
Shrayas <shrayasr@gmail.com>
2014-07-11 20:55:01 +0530

    Factorial, yo!

--- src/soomemath.scala
+++ src/soomemath.scala
@@ -4,8 +4,13 @@ object soomemath {
     if (y == 0) x else gcd(y, x%y)
   }
 
+  def factorial(x: Integer): Integer = {
+    if (x==0) 1 else (x*factorial(x-1))
+  }
+
   def main(args: Array[String]) {
     println(gcd(48,18))
+    println(factorial(5))
   }
 
 }

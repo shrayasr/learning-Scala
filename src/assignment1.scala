c93a7fbbbdff3600849e6a095bc14e41cea03fdd
Shrayas <shrayasr@gmail.com>
2014-07-13 11:25:11 +0530

    Fixing edge case with balancing
    - Can't close a bracket without it being open first

--- src/assignment1.scala
+++ src/assignment1.scala
@@ -21,7 +21,8 @@ object assignment1 {
     def balanceIter(accCount: Integer, chars: List[Char]): Integer = {
       if (chars.isEmpty) accCount
       else if (chars.head == '(') balanceIter(accCount+1, chars.tail)
-      else if (chars.head == ')') balanceIter(accCount-1, chars.tail)
+      else if (chars.head == ')' && accCount==0) 1
+      else if (chars.head == ')' && accCount>0) balanceIter(accCount-1, chars.tail)
       else balanceIter(accCount, chars.tail)
     }
 
@@ -35,6 +36,7 @@ object assignment1 {
     val string2 = "I told him (that it’s not (yet) done). (But he wasn’t listening)"
     val string3 = ":-)"
     val string4 = "())("
+
     print(balance(string1.toList))
     print(balance(string2.toList))
     print(balance(string3.toList))

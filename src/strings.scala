0c7142541085119b749a7941484f381608853602
Shrayas <shrayasr@gmail.com>
2014-07-23 08:14:09 +0530

    Learning soome strings

---
+++ src/strings.scala
@@ -0,0 +1,10 @@
+object strings {
+
+  def learnListChars(string: List[Char]) = print(string)
+
+  def main(args: Array[String]) {
+    val string = "Hello"
+    learnListChars(string.toList)
+  }
+
+}
